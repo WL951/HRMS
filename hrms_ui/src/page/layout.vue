886840116b720baf093d1a468125c27d3966dab9
WL <1281577501@qq.com>
2021-10-07 21:35:02 +0800

    'Fix:划分区域（头部、菜单栏、主体）.'

--- hrms_ui/src/page/layout.vue
+++ hrms_ui/src/page/layout.vue
@@ -21,22 +21,35 @@
 </script>
 
 <style>
+  .el-container{
+    position: absolute;
+    width: 100%;
+    top: 0px ;
+    left: 0 ;
+    bottom: 0;
+  }
   .el-header{
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
+    padding: 0;
+    z-index: 1000;
+    background-color: #42b983;
+  }
+  /*header菜单需要靠右的添加.fr即可(如：<el-menu-item class="fr" index="3">消息中心</el-menu-item>)*/
+  .el-header .fr{
+    float: right;
+  }
+  .el-header, .el-menu{
+    border-bottom: none;
+  }
+  .el-aside, .el-main{
+    padding-top: 60px;
   }
   .el-aside{
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
+    background: #545c64;
+  }
+  .el-aside, .el-menu{
+    border-right: none;
   }
   .el-main{
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
+    background-color: bisque;
   }
 </style>
